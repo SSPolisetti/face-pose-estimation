50b662d7aed04d5d67e94265ccaf6472cf426cdc
Sasanka Polisetti <83924431+SSPolisetti@users.noreply.github.com>
2023-03-16 19:15:09 -0400

    added .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/sasan/OneDrive/Desktop/Learning/Open CV/pose_estimation
-# Build directory: C:/Users/sasan/OneDrive/Desktop/Learning/Open CV/pose_estimation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
