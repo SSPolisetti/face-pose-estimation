61e7ec20e6ee6c3feddd45ff4e90fccba8a84d53
Sasanka Polisetti <83924431+SSPolisetti@users.noreply.github.com>
2023-03-16 12:12:30 -0400

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sasan/OneDrive/Desktop/Learning/Open CV/pose_estimation
+# Build directory: C:/Users/sasan/OneDrive/Desktop/Learning/Open CV/pose_estimation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
