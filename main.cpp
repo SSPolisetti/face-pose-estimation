24ac901c91d94e4b6f01afcc773c2bdcbefcd923
Sasanka Polisetti <83924431+SSPolisetti@users.noreply.github.com>
2023-03-16 21:26:11 -0400

    videocaputure

--- main.cpp
+++ main.cpp
@@ -2,10 +2,42 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv) {
 
-    
+    VideoCapture cap(0);
 
+    if (cap.isOpened() == false) {
+        cout << "Can't open camera\n";
+        cin.get();
+        return -1; 
+    }
 
+    double dWidth = cap.get(CAP_PROP_FRAME_WIDTH);
+    double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT);
+
+    namedWindow("Camera Feed");
+
+    while (true) {
+        Mat frame;
+
+        bool bSuccess = cap.read(frame);
+
+        if (bSuccess = false) {
+            cout << "Camera Disconnected\n";
+            cin.get();
+            break;
+        }
+
+        imshow("Camera Feed", frame);
+
+        if (waitKey(10) == 27) {
+            cout << "Esc pressed\n";
+            break;
+        } 
+
+    }
+
+    return 0;
 }
